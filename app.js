05d556d06892bffccbaf5fa71fff604ab5bd82c9
Steven Bryant <stevelad_83@yahoo.com>
2022-11-08 14:44:07 -0500

    adding form eventlistener

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /* Imports */
 
+import { renderPest } from './render-utils.js';
+
 /* Get DOM Elements */
 const pestsListEl = document.querySelector('.pests');
 const formEl = document.querySelector('form');
@@ -32,6 +34,35 @@ let killCount = 0;
 
 /* Events */
 
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(formEl);
+
+    const pestName = data.get('pest-name');
+
+    const newPest = {
+        id: currentId,
+        name: pestName,
+        hp: Math.ceil(Math.random() * 5),
+    };
+    currentId++;
+    pests.push(newPest);
+    displayPests();
+});
+
 /* Display Functions */
 
+function displayPests() {
+    pestsListEl.textContent = '';
+
+    for (let pest of pests) {
+        const pestEl = renderPest(pest);
+        pestEl.addEventListener('click', () => {
+            pestClickHandler(pest);
+        });
+        pestsListEl.append(pestEl);
+    }
+}
+
 // (don't forget to call any display functions you want to run on page load!)
+displayPests();
