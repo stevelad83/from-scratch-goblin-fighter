716bcb4a8687cd5bee5f9503d27b19b01fac97f9
Steven Bryant <stevelad_83@yahoo.com>
2022-11-08 12:36:40 -0500

    setting state

--- app.js
+++ app.js
@@ -8,6 +8,28 @@ const killedPestsEl = document.querySelector('#kill-count');
 const exterminatorImgEl = document.querySelector('#exterminator-img');
 /* State */
 
+const pests = [
+    {
+        id: 1,
+        name: 'Creepy',
+        hp: 1,
+    },
+    {
+        id: 2,
+        name: 'Crawly',
+        hp: 2,
+    },
+    {
+        id: 3,
+        name: 'Stinky',
+        hp: 3,
+    },
+];
+
+let currentId = 4;
+let exterminatorHP = 10;
+let killCount = 0;
+
 /* Events */
 
 /* Display Functions */
