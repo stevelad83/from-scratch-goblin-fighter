c3e8a5d6a6c016a6101da278c50f6e518c591be6
Steven Bryant <stevelad_83@yahoo.com>
2022-11-08 12:28:51 -0500

    grabbing DOM elements

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const pestsListEl = document.querySelector('.pests');
+const formEl = document.querySelector('form');
+const exterminatorHPEl = document.querySelector('#exterminator-hp');
+const killedPestsEl = document.querySelector('#kill-count');
+const exterminatorImgEl = document.querySelector('#exterminator-img');
 /* State */
 
 /* Events */
